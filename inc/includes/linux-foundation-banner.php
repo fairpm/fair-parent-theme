57660ec05a96e208d7b7d833660926b2ec2028a1
Joe Dolson <design@joedolson.com>
2025-06-27 09:23:25 -0500

    Should be an aside, so content is contained in a landmark.
    Signed-off-by: Joe Dolson <design@joedolson.com>

--- inc/includes/linux-foundation-banner.php
+++ inc/includes/linux-foundation-banner.php
@@ -12,12 +12,12 @@ namespace Fair_Parent;
  */
 function fair_linux_banner() {
 	?>
-	<div id="lf-header">
+	<aside id="lf-header">
 		<div class="container">
 			<a href="https://www.linuxfoundation.org/projects">
 				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/svg/lfprojects_banner_other.svg" alt="The Linux Foundation Projects">
 			</a>
 		</div>
-	</div>
+	</aside>
 	<?php
 }
