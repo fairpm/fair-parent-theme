c5968e510606f43dd64c80c84df5f8bfef75dcc4
Joe Dolson <design@joedolson.com>
2025-06-27 10:46:10 -0500

    Add visible site name
    Signed-off-by: Joe Dolson <design@joedolson.com>

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -14,8 +14,8 @@ $description = get_bloginfo( 'description', 'display' );
 
   <p class="site-title">
     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-      <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
-      <?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
+		<?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
+		<?php bloginfo( 'name' ); ?>
     </a>
   </p>
 
